23f46005ca11469d4e869e8ce5b68d34d0931044
Obiora Okeke <obioraokeke03@gmail.com>
2023-06-26 19:13:15 +0000

    We were able to access the spotify api and get back a list of related artists

--- spotify_recs.py
+++ spotify_recs.py
@@ -1,4 +1,5 @@
 import requests
+import pprint
 
 client_id = "ce303767105943e9b563c582c546bcdf"
 client_secret = "4f77f234a135413787ba25237ed8e819"
@@ -13,4 +14,12 @@ auth_response = requests.post(AUTH_URL, {
 
 auth_response_data = auth_response.json()
 
-print(auth_response_data)
+access_token = auth_response_data['access_token']
+
+headers = {'Authorization': 'Bearer {token}'.format(token=access_token)}
+
+
+BASE_URL = 'https://api.spotify.com/v1/'
+artist_id = input('Enter artist id: ')
+r = requests.get(BASE_URL + 'artists/' + artist_id + '/related-artists', headers=headers)
+pprint.pprint(r.json())
