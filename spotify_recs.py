c5b137eddc5c3de8c472eb318359a604ccd9b5cc
Obiora Okeke <obioraokeke03@gmail.com>
2023-06-26 20:29:52 +0000

    obtained of 20 related artists and just show the name, link, and popularity

--- spotify_recs.py
+++ spotify_recs.py
@@ -1,5 +1,9 @@
 import requests
+import pandas as pd
+import sqlalchemy as db
 import pprint
+
+pd.set_option('max_colwidth', None)
 
 client_id = "ce303767105943e9b563c582c546bcdf"
 client_secret = "4f77f234a135413787ba25237ed8e819"
@@ -20,6 +24,22 @@ headers = {'Authorization': 'Bearer {token}'.format(token=access_token)}
 
 
 BASE_URL = 'https://api.spotify.com/v1/'
-artist_id = input('Enter artist id: ')
+artist_id = "0eDvMgVFoNV3TpwtrVCoTj"#input('Enter artist id: ')
 r = requests.get(BASE_URL + 'artists/' + artist_id + '/related-artists', headers=headers)
-pprint.pprint(r.json())
+
+
+data = r.json()
+
+
+dataframe_name = pd.DataFrame.from_dict(data['artists'])
+
+dataframe_name['external_urls'] = dataframe_name['external_urls'].map(lambda x: x.get('spotify', 'N/A'))
+actual_data_frame = dataframe_name[['name', 'external_urls','popularity']]
+
+engine = db.create_engine('sqlite:///actual_data_frame.db')
+actual_data_frame.to_sql('table_name', con=engine, if_exists='replace', index=False)
+
+with engine.connect() as connection:
+   query_result = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()
+   print(pd.DataFrame(query_result))
+
