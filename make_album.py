2501432445a47aa1babbfbcad738ef60704095d5
anisha60 <93204813+anisha60@users.noreply.github.com>
2023-07-11 16:54:32 -0400

    reformat

--- make_album.py
+++ make_album.py
@@ -5,6 +5,16 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import spotipy.util as util
 
+def create_playlist(username, playlist_name, songs):
+    user = sp.user(username)
+    playlist = sp.user_playlist_create(username, playlist_name, public=True, collaborative=False, description = 'recs')
+    playlist_id = playlist['id']
+    track_uris = songs['uri'].tolist()
+    sp.playlist_add_items(playlist_id, track_uris)
+
+    print(f"Playlist '{playlist_name}' created successfully with {len(songs)} songs.")
+
+
 username = input("Enter your Spotify username: ")
 playlist_name = input("Enter the playlist name: ")
 
@@ -17,14 +27,5 @@ if token:
     sp = spotipy.Spotify(auth=token)
 else:
     print('cannot get token')
-
-def create_playlist(username, playlist_name, songs):
-    user = sp.user(username)
-    playlist = sp.user_playlist_create(username, playlist_name, public=True, collaborative=False, description = 'recs')
-    playlist_id = playlist['id']
-    track_uris = songs['uri'].tolist()
-    sp.playlist_add_items(playlist_id, track_uris)
-
-    print(f"Playlist '{playlist_name}' created successfully with {len(songs)} songs.")
-
+    
 create_playlist(username, playlist_name, songs)
