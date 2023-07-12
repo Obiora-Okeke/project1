c55c5224e797ca3251b7b7dd07dc252e6d26c78d
anisha60 <93204813+anisha60@users.noreply.github.com>
2023-07-12 14:28:11 -0400

    save current

--- make_album.py
+++ make_album.py
@@ -4,6 +4,16 @@ import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import spotipy.util as util
+# from flask import Flask, render_template, url_for, flash, redirect, request
+
+# app = Flask(__name__)
+
+# @app.route('/')
+def username_playlistname():
+    username = input("Enter your Spotify username: ")
+    playlist_name = input("Enter the playlist name: ")
+    return username, playlist_name
+
 
 def create_playlist(username, playlist_name, songs):
     user = sp.user(username)
@@ -14,18 +24,25 @@ def create_playlist(username, playlist_name, songs):
 
     print(f"Playlist '{playlist_name}' created successfully with {len(songs)} songs.")
 
-
-username = input("Enter your Spotify username: ")
-playlist_name = input("Enter the playlist name: ")
-
-token = util.prompt_for_user_token(username=username, 
-                                    scope=scope, 
-                                    client_id=CLIENT_ID,
-                                    client_secret=CLIENT_SECRET, 
-                                    redirect_uri='http://example.com/')
-if token:
-    sp = spotipy.Spotify(auth=token)
-else:
-    print('cannot get token')
+# main driver function
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application
+    # on the local development server.
+    # username = input("Enter your Spotify username: ")
+    # playlist_name = input("Enter the playlist name: ")
+    username, playlist_name = username_playlistname()
+    token = util.prompt_for_user_token(username=username, 
+                                        scope=scope, 
+                                        client_id=CLIENT_ID,
+                                        client_secret=CLIENT_SECRET, 
+                                        redirect_uri='http://example.com/')
+    if token:
+        sp = spotipy.Spotify(auth=token)
+    else:
+        print('cannot get token')
+        
+    create_playlist(username, playlist_name, songs)
+    # app.add_url_rule('/', create_playlist)
+    # app.run()
     
-create_playlist(username, playlist_name, songs)
