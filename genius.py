a073fdd0761c7b4de91ffdf7a2bbe11395e2b402
anisha60 <93204813+anisha60@users.noreply.github.com>
2023-07-19 15:23:23 -0400

    formatted lyrics and get annotation info

--- genius.py
+++ genius.py
@@ -18,6 +18,7 @@ headers = {'Authorization': 'Bearer {token}'.format(token=access_token)}
 
 genius = Genius(access_token)
 songs = pd.DataFrame()
+genius_song_ids = []
 def create(artist_name = 'pop'):
     global songs
     dat = api_call(artist_name)
@@ -35,55 +36,49 @@ def create(artist_name = 'pop'):
     # x = create_playlist(username, playlist_name, songs)
 
 def get_lyrics(x):
-#     URL = 'https://genius.com/Roddy-ricch-the-box-lyrics'
-#     page = requests.get(URL, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_4_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.15'})
-#     html = BeautifulSoup(page.text, "html_parser")
-#     lyrics = html.find("div", class_="lyrics").get_text()
-    # add = genius.search_song(x).api_path
-    # querystring = BASE_URL + add
-    # print(querystring)
-    # req = urllib.request.Request(querystring, headers=headers)
-    # req.add_header("Authorization", "Bearer " + access_token)
-    # req.add_header("User-Agent", "")
-    # opener = urllib.request.build_opener()
-    # resp = urllib3.urlopen(req, timeout=3)
-    # raw = resp.read()
-    # response = opener.open(req)
-    # print(response.json())
-    # print(vars(response))
-    # raw = response.data
-    # print(raw)
-    # print(vars(raw))
-    # json_obj = json.loads(raw)['response']['lyrics']
-    # print(raw)
-    # json_obj = json.loads(raw)['response']['']
+    global genius_song_ids
+    URL = 'https://genius.com/Roddy-ricch-the-box-lyrics'
+    page = requests.get(URL, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_4_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.15'})
+    soup = BeautifulSoup(page.content, "html.parser")
+    text = soup.select_one('div[class^="Lyrics__Container"], .lyrics').get_text(strip=True, separator='\n')
+    print(text)
 
-    # url = BASE_URL + add
-    # print(url)
-    # response = requests.get(url = url)
-    # html = bs(response.text, "html.parser")
-    # # print(html)
-    # print(html.find("div", class_="lyrics"))
-    # lyrics = html.find("div", class_="lyrics").get_text()
-    # return lyrics
-    lyrics = genius.search_song(x).lyrics
-    formatted_lyrics = lyrics.replace('\r', '').replace('[', '').replace(']', '').strip()
-    formatted_lyrics = f"""{formatted_lyrics}"""
+    song = genius.search_song(x)
+    genius_song_ids.append(song.id)
+    # lyrics = song.lyrics
+    # formatted_lyrics = lyrics.replace('\r', '').replace('[', '').replace(']', '').strip()
+    # formatted_lyrics = f"""{formatted_lyrics}"""
+    return text
 
-    return formatted_lyrics
+def get_annotations(x):
+    request = genius.referents(song_id=x, per_page=50)
+    # print('requests:', request)
+    annotations = [y for x in request['referents'] for y in x['annotations']]
+    fragments = [y for x in request['referents'] for y in x['fragment']]
+    annotation_dict = {}
+    count = 0
+    for annotation in annotations:
+        fragment = fragments[0]
+        annotation_dict[annotation['id']] = [fragment, annotation['body']]
+        count +=1
+    print(annotation_dict)
+    return annotation_dict
+
 
     
 
 create()
-# print(list(songs.columns))
-# response = genius.search_song(songs.iloc[0]['song'])
-# print()
-# print(vars(response))
-# print(global_songs)
-# get_lyrics(songs.iloc[0]['song'])
+
+#TO CREATE ANNOTATION
+# annotation = input('Create annotation: ')
+#fragment: referent/ lyric portion being annotated
+# genius.create_annotation(annotation, raw_annotable= '/lyrics', fragment = '')
+
 songs['lyrics'] = songs['song'].apply(lambda x: [get_lyrics(x)])
+songs['genius_id'] = genius_song_ids
+songs['annotations'] = songs['genius_id'].apply(lambda x: [get_annotations(x)])
 # songs['formatted_lyrics'] = songs['song'].apply(lambda x: [genius.search_song(x).lyrics])
-print(songs)
+# print(songs)
 
 
 
